e6ead1a0aeff982d3e6361c3f453cf7ba479065e
unknown <rdethlefs850@wit-xdvm-021.MILLARDPS.NET>
2013-02-27 13:34:36 -0600

    Groups now count contacts within the group and styling was improved

--- pages/list_contacts.php
+++ pages/list_contacts.php
@@ -13,7 +13,7 @@
 		$conn = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
 		//Query DB
-		$sql = 'SELECT * FROM contacts ORDER BY contact_lastname, contact_firstname';
+		$sql = 'SELECT * FROM contacts LEFT JOIN groups ON contacts.group_id=groups.group_id ORDER BY contact_lastname,contact_firstname';
 		$results = $conn->query($sql);
 
 		//Loop over result set, displaying contacts
@@ -24,6 +24,7 @@
 				<td><?php echo $contact_firstname ?> <?php echo $contact_lastname?></td>
 				<td><a href="mailto"><?php echo $contact_email ?></a></td>
 				<td><?php echo format_phone($contact_phone) ?></td>
+				<td><span class="label label-info"><?php echo $group_name ?></span></td>
 				<td>
 					<a class="btn btn-warning" href="./?p=form_edit_contact&id=<?php echo $contact_id?>"><i class="icon-edit icon-white"></i></a>
 					<form class="form-inline" action="actions/delete_contact.php" method="post">
--- pages/list_groups.php
+++ pages/list_groups.php
@@ -1,20 +1,21 @@
 <h2>Groups</h2>
-<ul>
+<ul class="nav nav-pills nav-stacked">
 <?php
 
 //Connect to DB
 $conn = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
 //Query DB
-$sql = "SELECT * FROM groups ORDER BY group_name";
+$sql = 'SELECT groups.*, COUNT(contact_id) AS num_contacts FROM groups LEFT JOIN contacts ON groups.group_id=contacts.group_id GROUP BY groups.group_id ORDER BY group_name';
 $results = $conn->query($sql);
 
 while(($group = $results->fetch_assoc()) != null) {
 	extract($group);
 	?>
-	<li><a href="./?p=group&id=<?php echo $group_id ?>"><?php echo $group_name ?></a></li>
+	<li><span class="pull-right badge"><?php echo $num_contacts ?></span><a href="./?p=group&id=<?php echo $group_id ?>"><?php echo $group_name ?></a></li>
 	<?php 
 	
 }
-$conn->close(); ?>
+$conn->close();
+ ?>
 </ul>
